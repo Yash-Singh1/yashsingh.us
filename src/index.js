345f3730e473b142c01ed1d67fe167914ba78d00
Yash-Singh1 <saiansh2525@gmail.com>
2021-10-24 20:19:34 -0700

    Add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { render } from 'react-dom';
 import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import './styles/index.css';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 render(
   <Provider store={store}>
