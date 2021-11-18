412fa99bf7b3f680972e2b88a6f48ff36ed7eb98
Yash Singh <saiansh2525@gmail.com>
2021-11-18 11:28:36 -0800

    Add remark-gfm

--- build.js
+++ build.js
@@ -7,31 +7,33 @@ const svgrPlugin = require('esbuild-plugin-svgr');
 const dev = process.argv[2] === 'dev';
 
 import('xdm/esbuild.js').then(({ default: xdm }) => {
-  esbuild
-    .build({
-      entryPoints: ['src/index.js'],
-      bundle: true,
-      outdir: 'public/',
-      splitting: true,
-      format: 'esm',
-      plugins: [ImportGlobPlugin(), postCssPlugin(require('./postcss.config.js')), xdm(), svgrPlugin()],
-      minify: true,
-      loader: {
-        '.js': 'jsx'
-      },
-      watch: dev,
-      inject: ['./src/inject/react-shim.js'],
-      logLevel: dev ? 'info' : 'error',
-      minify: !dev
-    })
-    .catch((e) => console.error(e.message));
+  import('remark-gfm'.then(({ default: remarkGfm }) => {
+    esbuild
+      .build({
+        entryPoints: ['src/index.js'],
+        bundle: true,
+        outdir: 'public/',
+        splitting: true,
+        format: 'esm',
+        plugins: [ImportGlobPlugin(), postCssPlugin(require('./postcss.config.js')), xdm({ remarkPlugins: [remarkGfm] }), svgrPlugin()],
+        minify: true,
+        loader: {
+          '.js': 'jsx'
+        },
+        watch: dev,
+        inject: ['./src/inject/react-shim.js'],
+        logLevel: dev ? 'info' : 'error',
+        minify: !dev
+      })
+      .catch((e) => console.error(e.message));
 
-  if (dev) {
-    liveServer.start({
-      port: 5678,
-      root: 'public',
-      file: 'index.html',
-      logLevel: dev ? 1 : 0
-    });
-  }
+    if (dev) {
+      liveServer.start({
+        port: 5678,
+        root: 'public',
+        file: 'index.html',
+        logLevel: dev ? 1 : 0
+      });
+    }
+  });
 });
