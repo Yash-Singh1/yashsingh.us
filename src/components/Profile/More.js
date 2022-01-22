9b2c11f6ae6d33d0401758be526a5681f37ff2c9
Yash-Singh1 <saiansh2525@gmail.com>
2022-01-22 13:09:37 -0800

    chore(More): increase x padding

--- src/components/Profile/More.js
+++ src/components/Profile/More.js
@@ -15,7 +15,7 @@ function More({ children, onShown = () => {}, onHidden = () => {} }) {
       <div id='show-more-container' className={`text-center mt-5${moreShown ? ' hidden' : ''}`}>
         <button
           id='show-more'
-          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-blue-700 hover:border-blue-500 rounded-full cursor-pointer outline-none'
+          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-8 border-blue-700 hover:border-blue-500 rounded-full cursor-pointer outline-none'
           onClick={() => showMore(true)}
           type='button'
         >
@@ -26,7 +26,7 @@ function More({ children, onShown = () => {}, onHidden = () => {} }) {
       <div id='show-less-container' className={`text-center mt-5${moreShown ? '' : ' hidden'}`}>
         <button
           id='show-less'
-          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-blue-700 hover:border-blue-500 rounded-full cursor-pointer outline-none'
+          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-8 border-blue-700 hover:border-blue-500 rounded-full cursor-pointer outline-none'
           onClick={() => {
             onHidden();
             showMore(false);
