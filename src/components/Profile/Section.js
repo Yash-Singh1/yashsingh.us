d5a3096b15af1f0f9d2e9d51c40bcebeefe2ea87
Yash-Singh1 <saiansh2525@gmail.com>
2022-01-22 13:03:23 -0800

    fix(Section): vh instead of %

--- src/components/Profile/Section.js
+++ src/components/Profile/Section.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 function Section({ title, children }) {
   const hash = title.toLowerCase().replace(' ', '-');
   return (
-    <div className='min-h-full section' id={hash}>
+    <div className='full-min-h section' id={hash}>
       <div data-aos='slide-right'>
-        <br />
-        <br />
         <span
           className='subheading'
           onClick={() => {
