c5afe2f644d45c5d9ceb1ca35467dbb84776b87a
Yash-Singh1 <saiansh2525@gmail.com>
2022-03-17 21:08:31 -0700

    chore: fix syntax err from search and replace

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -5,7 +5,7 @@ import badgeColorMap from '../../helpers/badgeColorMap.json';
 function Post({ ...frontmatter }) {
   const LinkComponent = frontmatter.link ? 'a' : Link;
   const linkProps = frontmatter.link
-    ? { href: frontmatter.link, target: '_blank' rel="noreferrer noopener" }
+    ? { href: frontmatter.link, target: '_blank' }
     : { to: `/blog/post/${frontmatter.filename}` };
 
   return (
