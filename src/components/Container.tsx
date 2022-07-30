1e4cf3447fd9ec388c2021a6b447fdb77006c5be
Yash-Singh1 <saiansh2525@gmail.com>
2022-07-30 12:21:34 -0500

    fix: high dpi bg

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,11 +7,7 @@ interface ContainerProps {
 
 function Container({ children, className }: ContainerProps) {
   return (
-    <div
-      className={`py-12 lg:py-16 xl:py-24 px-6 md:px-12 lg:px-24 max-w-screen-2xl mx-auto ${
-        className || ''
-      }`}
-    >
+    <div className={`py-12 lg:py-16 xl:py-24 px-6 md:px-12 lg:px-24 mx-auto ${className || ''}`}>
       {children}
     </div>
   );
