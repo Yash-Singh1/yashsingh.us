975b350f76126e3d3f8b72a43f68e2acd81e0bbc
Yash-Singh1 <saiansh2525@gmail.com>
2022-08-09 16:11:42 -0500

    chore: update uses

--- src/components/Post/Markdown_Components/Paragraph.tsx
+++ src/components/Post/Markdown_Components/Paragraph.tsx
@@ -4,7 +4,7 @@ const Paragraph = (props: any) => {
   return (
     <Text
       {...Object.assign({ ref: undefined }, props)}
-      className='text-base sm:text-lg md:text-xl'
+      className='text-base sm:text-lg md:text-xl max-w-[clamp(45ch,100%,75ch)]'
     />
   );
 };
