ca8f9544657f35714e7c55be1abd21f7de1b0963
Yash Singh <saiansh2525@gmail.com>
2023-11-08 20:32:50 -0800

    chore: try using script snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import { type Metadata } from 'next';
 import type React from 'react';
 import Footer from '../components/Footer';
 import { AOSProvider } from '../components/AOSProvider';
-import { PostHogProvider } from '../components/PostHogProvider';
+import js from '../helpers/passthrough';
 import '../styles/global.scss';
 import '../styles/loader.scss';
 import '@fontsource/baloo-bhai-2/600.css';
 import '@fontsource/baloo-bhai-2/700.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   authors: [
@@ -37,7 +38,19 @@ export default function RootLayout({
         <Footer />
       </body>
       <AOSProvider />
-      <PostHogProvider />
+      <Script
+        id='gtag-script'
+        strategy='worker'
+        dangerouslySetInnerHTML={{
+          __html: js`
+      <script>
+        !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+        if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
+          posthog.init('wavhjmW6gdO6F3TuUJ2nEo3nFQYYhnDdUdHBpumycSg_chp'.reverse(),{api_host:'https://app.posthog.com'});
+        }
+      </script>`,
+        }}
+      />
     </html>
   );
 }
