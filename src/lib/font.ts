0b569ce62498f29669a6342e660dc9559b8ab72f
Yash Singh <saiansh2525@gmail.com>
2024-09-05 21:44:06 -0700

    fix: code style

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -3,5 +3,5 @@ import { Baloo_Bhai_2 } from 'next/font/google';
 export const font = Baloo_Bhai_2({
   subsets: ['latin'],
   weight: ['600', '700'],
-  variable: '--font-baloo-bhai'
+  variable: '--font-baloo-bhai',
 });
